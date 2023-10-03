f2a4261ff090c2f37d25f2e2bbddb232d476290c
oBoisteanu <oboisteanu@gmail.com>
2023-10-03 21:14:06 +0300

    Initial commit HomeWork_4.1

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,13 @@
+package edu.tekwill.academy.computer;
+import edu.tekwill.academy.computer.components.*;
+import edu.tekwill.academy.computer.peripherals.*;
+
+public class Computer {
+    Processor processor = new Processor ();
+    Motherboard motherboard = new Motherboard();
+    HardDrive hardDrive = new HardDrive();
+    Monitor monitor = new Monitor();
+    Mouse mouse = new Mouse();
+    Keyboard keyboard = new Keyboard();
+
+}
